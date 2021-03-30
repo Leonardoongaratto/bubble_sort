eca340a3a337d34444308c076f8902675177e777
Leonardoongaratto <leonardocongaratto1@gmail.com>
2021-03-30 09:23:11 -0300

    bublle_sort

---
+++ ordenaAlfabeto.py
@@ -0,0 +1,17 @@
+def bubble_sort(arrayWord):
+
+    for j in range(len(arrayWord) - 1):
+        for s in range(len(arrayWord) - 1):
+            if (arrayWord[s] > arrayWord[s+1]):
+                arrayWord[s], arrayWord[s+1] = arrayWord[s+1], arrayWord[s]
+
+    return arrayWord
+
+print("Ordena letras em ordem alfabeta")
+word = input("Digite uma palavra")
+
+arrayWord = []
+for w in word:
+    arrayWord.append(w)
+
+print(bubble_sort(arrayWord))
